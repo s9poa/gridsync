2349a7cd45f0f4050228e24aa7c6ee81d025d8b2
s9poa <s9poah@gmail.com>
2025-08-19 19:05:33 -0400

    Added onView Animations for Certain Elements

---
+++ javascript/global/onView.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const elements = document.querySelectorAll(".animate-onView");
+
+  const observer = new IntersectionObserver(
+    (entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.style.visibility = "visible";
+          entry.target.style.animation = "onView .275s alternate";
+          observer.unobserve(entry.target); // run once per element
+        }
+      });
+    },
+    { threshold: 0.1 } // triggers when 10% of element is visible
+  );
+
+  elements.forEach(el => {
+    el.style.visibility = "hidden"; // start hidden
+    observer.observe(el);
+  });
+});
